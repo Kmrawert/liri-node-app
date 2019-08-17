053578fca6d29adf63fcd12d1e48c3ed46e21ff2
Katie Rawert <kmrawert@yahoo.com>
2019-08-17 14:35:18 -0500

    added file structure

---
+++ liri.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+
+//Spotify commands: 
+`concert-this`
+`spotify-this-song`
+
+//OMDB
+`movie-this`
+
+//????
+`do-what-it-says`
